b55465e64abce99cde45d1e1f2ab71f6093a1f03
Kaivan Kamali <kxk302@psu.edu>
2023-12-04 08:49:41 -0500

    Updated file paths in script

--- scripts/split_fasta_file.sh
+++ scripts/split_fasta_file.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/mGorGor1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/output/Gorilla_gorilla/ -p chr
+python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/aggregate_fasta_files/mGorGor1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/input/Gorilla_gorilla/ -p chr
 
-python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/mPanPan1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/output/Pan_paniscus/ -p chr
+python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/aggregate_fasta_files/mPanPan1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/input/Pan_paniscus/ -p chr
 
-python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/mPanTro3.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/output/Pan_troglodytes/ -p chr
+python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/aggregate_fasta_files/mPanTro3.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/input/Pan_troglodytes/ -p chr
 
-python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/mPonAbe1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/output/Pongo_abelii/ -p chr
+python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/aggregate_fasta_files/mPonAbe1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/input/Pongo_abelii/ -p chr
 
-python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/mPonPyg2.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/output/Pongo_pygmaeus/ -p chr
+python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/aggregate_fasta_files/mPonPyg2.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/input/Pongo_pygmaeus/ -p chr
 
-python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/mSymSyn1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/output/Symphalangus_syndactylus/ -p chr
+python3 ./scripts/split_fasta_file.py -f ~/workspace/non-B_gfa/v2/input/aggregate_fasta_files/mSymSyn1.pri.cur.20231122.fasta -o ~/workspace/non-B_gfa/v2/input/Symphalangus_syndactylus/ -p chr
